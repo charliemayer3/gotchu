ddcc334b8106d523e3b57daba7ff82b2f723465f
Maxwell Cutri <mpcutri@gmail.com>
2018-06-05 15:39:42 -0600

    home page styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/Menu';
+import Home from './pages/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Menu/>
+        <Home/>
       </div>
     );
   }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+import "./Home.css";
+import React, { Component } from 'react';
+import Logo from '../../images/gotchuLogo.png';
+import OverlayMenu from 'react-overlay-menu';
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    };
+  }
+ 
+  render() {
+    return (
+      <div className="homeBody">
+
+        <div className="logoContainer">
+          <img id="logo" src={Logo} />
+        </div>
+
+      	<div id="welcomeText">
+          <h1>
+            gotchu
+          </h1>
+        </div>
+
+      </div>
+    
+    );
+  }
+}
+
+export default Home;
