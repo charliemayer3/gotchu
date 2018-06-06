d09c2aec1b0a1bd2e1b494bfa4d8ce13eee1b676
Maxwell Cutri <mpcutri@gmail.com>
2018-06-06 15:07:43 -0600

    login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,1 +1,29 @@
-Login.js
+import React, { Component } from 'react'
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Redirect } from 'react-router-dom'
+import { Button } from 'reactstrap';
+import "./Login.css";
+import { Alert } from 'reactstrap';
+
+
+class Login extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			redirectTo: null,
+			loggedIn: true,
+			alert: false
+		}
+		// this.googleSignin = this.googleSignin.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
