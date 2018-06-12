8676e9d7d412a15279e93a3b7a0ef20968ebd940
Maxwell Cutri <mpcutri@gmail.com>
2018-06-12 16:12:00 -0600

    mission page and animation

--- client/src/pages/Mission/Mission.js
+++ client/src/pages/Mission/Mission.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { Button } from 'reactstrap';
 import "./Mission.css";
+import textBack from '../../images/textBack.png';
 // import { Alert } from 'reactstrap';
 
 
@@ -17,9 +18,15 @@ class Mission extends Component {
 			return (
 				<div>
 					<div className="missionContainer">
-					<h4>
-						
+
+					<h4 id='missionText'>
+						“Gotchu, the Blockchain based Giving Platform that localizes giving through our secure IoT technology.”
 					</h4>
+					</div>
+
+					<div class="ocean">
+					  <div class="wave"></div>
+					  <div class="wave"></div>
 					</div>
 				</div>
 			)
