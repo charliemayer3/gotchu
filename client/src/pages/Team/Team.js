8fe47cbe1768064456a0a8a7530ad9e1fb880ee6
Maxwell Cutri <mpcutri@gmail.com>
2018-07-24 11:33:01 -0600

    hotjar and heroku build

--- client/src/pages/Team/Team.js
+++ client/src/pages/Team/Team.js
@@ -10,10 +10,6 @@ import Jamison from '../../images/jamison.png';
 import Charlie from '../../images/charlie.jpg';
 import Max from '../../images/max.jpg';
 import textBack from '../../images/colorBack.png';
-// import { Alert } from 'reactstrap';
-
-
-
 
 class Team extends Component {
 	constructor() {
@@ -23,42 +19,33 @@ class Team extends Component {
 		}
 		this.nameArray = [
 			{ 
-				Name: "Don Bailey",
-				Title: "CEO / Founder",
-				Image: Don,
-				Bio: "Don has a history of ground-breaking research that has protected mobile users from worldwide tracking system, secured automobiles from remote attack, and mitigated crippling IoT risks."
+				name: "Don Bailey",
+				title: "CEO / Founder",
+				image: Don,
+				bio: "Don has a history of ground-breaking research that has protected mobile users from worldwide tracking system, secured automobiles from remote attack, and mitigated crippling IoT risks."
 			},
 			{ 
-				Name: "Robyn Tsukayama",
-				Title: "Chief of Operations",
-				Image: Robyn,
-				Bio: "Robyn is a diverse leader with experience in the automotive, consumer packaged goods, education, and social enterprise industries. She earned a B.S. in Mechanical Engineering from Kettering University, an MBA from Harvard Business School, and is a StartingBloc Fellow."
+				name: "Robyn Tsukayama",
+				title: "Chief of Operations",
+				image: Robyn,
+				bio: "Robyn is a diverse leader with experience in the automotive, consumer packaged goods, education, and social enterprise industries. She earned a B.S. in Mechanical Engineering from Kettering University, an MBA from Harvard Business School, and is a StartingBloc Fellow."
 			},
 			{ 
-				Name: "Jamison M Day, PhD.",
-				Title: "CEO / Founder",
-				Image: Jamison,
-				Bio: "Jamison is a Distinguished Data Scientist at LookingGlass Cyber Solutions, Inc. He served on a Supply Chain Security Team for the U.S. Director of National Intelligence. Jamison earned his Ph.D in Decision Science from Indiana University, and B.S. in Business Process Management."
+				name: "Charlie Mayer",
+				title: "Web Development",
+				image: Charlie,
+				bio: "Charlie is a Full Stack Web Developer and web development teacher, whose expertise lies deepest in the MERN stack. While developing in JavaScript and React is Charlie's happy place, he is versed in everything from CSS to PHP."
 			},
 			{ 
-				Name: "Charlie Mayer",
-				Title: "Web Development",
-				Image: Charlie,
-				Bio: "Charlie is a Full Stack Web Developer and web development teacher, whose expertise lies deepest in the MERN stack. While developing in JavaScript and React is Charlie's happy place, he is versed in everything from CSS to PHP."
-			},
-			{ 
-				Name: "Maxwell Cutri",
-				Title: "Web Development",
-				Image: Max,
-				Bio: "Maxwell is a Full Stack Web Developer with a passion for UX & UI. While JavaScript, React.js, and CSS3 are his most practiced languages, he enjoys developing custom web applications and encorporating his skills with Adobe Creative Suite and vector drawing software."
+				name: "Maxwell Cutri",
+				title: "Web Development",
+				image: Max,
+				bio: "Maxwell is a Full Stack Web Developer with a passion for UX & UI. While JavaScript, React.js, and CSS3 are his most practiced languages, he enjoys developing custom web applications and encorporating his skills with Adobe Creative Suite and vector drawing software."
 			},
 		]
 	}
 
-//objects Name, Title, Image, Bio
 // USE FLEXBOX TO STYLE EACH DIV
-
-
 	render() {
 			return (
 				<div className='teamBody'>
@@ -77,21 +64,21 @@ class Team extends Component {
 						{console.log(person)}
 						return (
 							<div>
-								<div id="don">
-									<div id='donPic'>
-										<img src={person.Image} />
+								<div id="team-member">
+									<div id='memberPic'>
+										<img src={person.image} />
 									</div>
-									<div id='donText'>
+									<div id='memberName'>
 										<span style={{ fontSize: '25px' }}>
-											{person.Name}
+											{person.name}
 										</span>
 											<br />
 										<span style={{ fontStyle: 'italic' }}>
-											{person.Title}
+											{person.title}
 										</span>
 											<br />
 										<span style={{  }}>
-											{person.Bio}
+											{person.bio}
 										</span>
 									</div>
 								</div>
